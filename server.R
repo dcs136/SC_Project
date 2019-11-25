68bf26d3fe9d9d74a7e96e48231905b169fa4239
dcs136 <dcs136@hotmail.com>
2019-11-25 10:57:30 -0500

    Calculate button

--- server.R
+++ server.R
@@ -1,27 +1,73 @@
-library(shiny)
-library(shinydashboard)
-library(spuRs)
-library(cmna)
+# library(shiny)
+# library(shinydashboard)
+# library(spuRs)
+# library(cmna)
+
 
 shinyServer(function(input, output){
-  
-  date_range <- reactive({
+ 
+  observeEvent(input$calculate,{
     
-    if (input$state_b ==  "log(x) + exp(-x)") {
-      
+    # Ifelse statements are used to link the functions.
+    
+    if (input$equation ==  "log(x) + exp(-x)") { 
       ftn <- function(x) 
-        return(x-log(x)+exp(-x))
+        return(log(x)^3 + exp(-x)^2)
+      
+      x0 <- input$num1
+      output$curve <- renderPlot({
+        curve(expr = ftn, col='blue', lwd=2, lty=2, xlim=c(-4,4))
+        abline(h=0)
+        abline(v=0)
+        
+        })
+      output$iteration <- renderPrint( 
+        fixedpoint(ftn, x0, tol = 1e-9, max.iter = 100))
     }
     
-    output$curve <- renderPlot({
-      curve(ftn)  
-    })
-    
-    x0 <- input$num1 
-    output$iteration <- renderPrint( 
-      fixedpoint(ftn, x0, tol = 1e-9, max.iter = 100))
-  }
-  )
-}
-)
-#ftn <- function(x) return(x-log(x)+exp(-x))
+    else if (input$equation == "cos(x) * x") { 
+      ftn <- function(x) 
+        return(cos(x))
+      
+      x0 <- input$num1
+      output$iteration <- renderPrint( 
+        fixedpoint(ftn, x0, tol = 1e-9, max.iter = 100))
+      
+      output$curve <- renderPlot({
+        curve(expr = ftn, col='blue', lwd=2, lty=2, xlim=c(-4,4))
+        abline(h=0)
+        abline(v=0)
+        
+      })
+    }
+  })
+})
+#   output$Area_ui_b <- renderUI({
+#     func_choices <- functions %>%
+#       filter(functions == input$equation) %>%
+#       select(functions)
+#   
+#   
+#   dataInput <- reactive({
+#     filter_dat <- functions[functions$Functions == input$equation, ]
+#     
+#   })
+#   
+#   #date_range <- reactive({
+#     
+#     # if (input$equation ==  "log(x) + exp(-x)") {
+#       
+#       # ftn <- function(x) 
+#       #   return(x-log(x)+exp(-x))
+#     # }
+#     output$curve <- renderPlot({
+#       curve(ftn)  
+#     })
+#     
+#     x0 <- input$num1 
+#     output$iteration <- renderPrint( 
+#       fixedpoint(ftn, x0, tol = 1e-9, max.iter = 100))
+#   }
+#   )
+# 
+# #ftn <- function(x) return(x-log(x)+exp(-x))
--- ui.R
+++ ui.R
@@ -2,25 +2,32 @@ dashboardPage(skin = "purple",
   dashboardHeader(title = "Root Finding App"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Root Finding", tabName = "root", icon = icon("home"))
+      menuItem("Root Finding", tabName = "root", icon = icon("bar-chart-o"))
     ),
     selectInput(inputId = 'equation',
                 selectize = FALSE,
                 size = 1,
                 label = 'Select a Function',
                 choices = unique(functions$Functions),
-                selected = c('CT')),
+                selected = c('log(x) + exp(-x)')),
     uiOutput('Area_ui_b'),
     numericInput("num1", "First Initial Point", value = 0, min = -100, max = 100, step = 0.1),
+   textInput("text", "Text Input: "),
    actionButton("calculate", "Find Root")
 ),
-  
   dashboardBody(
     tabItems(
       tabItem(tabName = "root")),
     fluidRow(
-      plotOutput('curve'),
-      htmlOutput('iteration')
+      box(
+        title = "Curve", background = "blue", solidHeader = TRUE,
+        collapsible = TRUE,
+        plotOutput('curve')),
+      box(
+        title = "Root Point", width = 4, background = "light-blue",
+        htmlOutput('iteration')
+      ),  
+      #htmlOutput('iteration')
     )
   )
 )
